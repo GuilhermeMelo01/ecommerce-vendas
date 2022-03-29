653c84e0e6bdae9a569bd7f763ae8b2c384f732e
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-03-29 16:38:43 -0300

    Initial commit

---
+++ src/main/java/com/guilhermemelo/course/SpringbootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermemelo.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUdemyApplication.class, args);
+	}
+
+}
