daa752964ddcd9cb6b604babe7a1a4c68c533ebf
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-03-31 16:24:46 -0300

    Add H2 database and Create Entity

--- src/main/java/com/guilhermemelo/course/domain/Categoria.java
+++ src/main/java/com/guilhermemelo/course/domain/Categoria.java
@@ -1,14 +1,24 @@
 package com.guilhermemelo.course.domain;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotNull
     private String name;
 
     public Categoria() {
