81d5b92e2b2af50f8bc0a4df0d53881ca0cac452
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-04-04 19:47:32 -0300

    Operação de instanciação

--- src/main/java/com/guilhermemelo/course/SpringbootUdemyApplication.java
+++ src/main/java/com/guilhermemelo/course/SpringbootUdemyApplication.java
@@ -1,13 +1,33 @@
 package com.guilhermemelo.course;
 
+import com.guilhermemelo.course.domain.Categoria;
+import com.guilhermemelo.course.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class SpringbootUdemyApplication {
+public class SpringbootUdemyApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootUdemyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		
+	}
 }
