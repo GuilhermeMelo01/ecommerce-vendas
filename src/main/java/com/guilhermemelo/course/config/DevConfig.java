d8f6550dee7ff618c1486009676b11a229ecc106
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-05-06 11:49:27 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/guilhermemelo/course/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.guilhermemelo.course.config;
+
+import com.guilhermemelo.course.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (strategy.equals("create")){
+            dbService.instantiateTestDatabase();
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
