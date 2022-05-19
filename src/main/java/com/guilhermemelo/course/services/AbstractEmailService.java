50e1313a7c7879ad32978098b9f69861e8b94d8d
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-05-19 10:30:01 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/guilhermemelo/course/services/AbstractEmailService.java
+++ src/main/java/com/guilhermemelo/course/services/AbstractEmailService.java
@@ -21,7 +21,7 @@ public abstract class AbstractEmailService implements EmailService {
     @Autowired
     private TemplateEngine templateEngine;
 
-    @Autowired
+    @Autowired(required = false)
     private JavaMailSender javaMailSender;
 
     @Override
