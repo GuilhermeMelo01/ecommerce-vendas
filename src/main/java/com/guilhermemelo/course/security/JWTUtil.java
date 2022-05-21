cd58b0248e0b018611181fddf9d461a0aaf8b507
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-05-21 11:22:55 -0300

    Implementando Autorizacao

--- src/main/java/com/guilhermemelo/course/security/JWTUtil.java
+++ src/main/java/com/guilhermemelo/course/security/JWTUtil.java
@@ -45,7 +45,7 @@ public class JWTUtil {
         }
         return null;
     }
-
+    
     private Claims getClaims(String token) {
         try{
             return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
